e56c9255b0dca82b9a2ef0f2aaa1a24849a9616d
Lukmanul <lukman988@hotmail.com>
2017-07-29 14:00:35 +0530

    Initial Commit

---
+++ LinkBook/LinkBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinkBook.Startup))]
+namespace LinkBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
